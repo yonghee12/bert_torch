5c155c51a49bc027f1a2d4c870bb55adf9f67a29
yonghee12 <suncar1000@gmail.com>
2020-09-21 18:39:13 +0900

    finished BERT input block v1

--- bert/blocks.py
+++ bert/blocks.py
@@ -2,5 +2,22 @@ import torch.nn as nn
 
 
 class BERTInputBlock(nn.Module):
-    def __init__(slef, vocab_size, hidden_dim=768, dropout=0.1):
-        raise NotImplementedError
+    def __init__(self, vocab_size, hidden_dim=768, max_seq=512, dropout=0.1, pad_idx=0):
+        """
+        BERT Input Block
+        The reason why segment embedding is initialized with 3 vocab size is because of the pad_idx
+        :param int vocab_size: corpus vocab size
+        :param hidden_dim: d_model in Transformer
+        :param max_seq: max sequence length
+        :param dropout: dropout probability
+        :param pad_idx: padding idx, default 0
+        """
+        super().__init__()
+        self.token_emb = nn.Embedding(vocab_size, hidden_dim, pad_idx)
+        self.positional_emb = nn.Embedding(max_seq + 1, hidden_dim, pad_idx)
+        self.segment_emb = nn.Embedding(3, hidden_dim, pad_idx)
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, x, x_seg):
+        x = self.token_emb(x) + self.positional_emb(x) + self.segment_emb(x_seg)
+        return self.dropout(x)
