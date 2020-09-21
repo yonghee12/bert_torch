ebda12b205d76a0c61bb1692488612947cab0994
yonghee12 <suncar1000@gmail.com>
2020-09-21 14:58:25 +0900

    added inputblock skeleton

--- bert.py
+++ bert.py
@@ -1,9 +1,9 @@
 from transformer.transformer import *
-
+from blocks import BERTInputBlock
 
 
 class BERTModel:
     def __init__(self, vocab_size, hidden_dim=768, max_seq_len=512, n_layers=12, n_heads=12, dropout=0.1, pad_idx=0):
-        self.input = BERTInputBlock(vocab_size)
+        self.input = BERTInputBlock(vocab_size, hidden_dim, dropout)
         self.encoders = nn.ModuleList(
             [TransformerEncoderBlock(n_layers, hidden_dim, hidden_dim * 4, n_heads, dropout) for _ in range(n_layers)])
---
+++ blocks/inputblock.py
@@ -0,0 +1,6 @@
+import torch.nn as nn
+
+
+class BERTInputBlock(nn.Module):
+    def __init__(slef, vocab_size, hidden_dim=768, dropout=0.1):
+        raise NotImplementedError
