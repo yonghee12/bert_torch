be3d75993b2f43856466517cd8f2150fb47a6d26
yonghee12 <suncar1000@gmail.com>
2020-09-21 18:38:51 +0900

    added main

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-# load trainer and train
+from torchsummary import summary
 
+from bert import *
+
+model = BERTModel(vocab_size=10000, hidden_dim=768, max_seq=512, n_layers=12, n_heads=12, dropout=0.1, pad_idx=0)
+print(model)
+
+summary(model, (512, 768))
