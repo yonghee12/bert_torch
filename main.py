a890ab9057050d8669f0acb66bc8a2e2fda8419f
yonghee12 <suncar1000@gmail.com>
2020-09-24 19:00:34 +0900

    .

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from torchsummary import summary
 
 from bert import *
 
-model = BERTModel(vocab_size=10000, hidden_dim=768, max_seq=512, n_layers=12, n_heads=12, dropout=0.1, pad_idx=0)
+model = BERTModel(vocab_size=1000, hidden_dim=768, max_seq=512, n_layers=12, n_heads=12, dropout=0.1, pad_idx=0)
 print(model)
 
-summary(model, (512, 768))
+# input: N, T
+summary(model, [(1, 512), (1, 512)])
